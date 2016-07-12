2906354fab31c6d660d406094b681767622b8aae
joyghosh <joy.ghosh@tiatros.com>
2016-07-12 11:21:41 +0530

    version 0.0.1

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-// module.exports = require('./lib/bitview.js');
 var bloom = require('./lib/bloomfilter.js');
 module.exports = bloom;
-// module.exports = require('./lib/fnv.js');
-// module.exports = require('./lib/jenkins.js');
